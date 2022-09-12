7ebaca67ad35010ca2ec4ef2db0a910aa3e74d5c
Highload <perepelkin-da@yandex.ru>
2022-09-12 15:01:48 +0600

    DZ4

--- torg/app/Providers/AppServiceProvider.php
+++ torg/app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Handlers\LoggerHandler;
-use App\Handlers\LoggerHandlerInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,7 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(LoggerHandlerInterface::class,LoggerHandler::class);
     }
 
     /**
